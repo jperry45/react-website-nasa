42ab08d7ee38a554c6dfb8feb89346f16c0f67a6
Joshua Perry <joshuacperry95@gmail.com>
2021-11-02 17:20:17 -0500

    Updates

---
+++ precache-manifest.d81409f134b25b5677177ebc25c89310.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a7e716d16dd54e4fcb50cc3102a739",
+    "url": "/react-website-nasa/index.html"
+  },
+  {
+    "revision": "735935c312b026542d78",
+    "url": "/react-website-nasa/static/css/2.9fca9f18.chunk.css"
+  },
+  {
+    "revision": "77a2fcf1145974d29292",
+    "url": "/react-website-nasa/static/css/main.6a12410e.chunk.css"
+  },
+  {
+    "revision": "735935c312b026542d78",
+    "url": "/react-website-nasa/static/js/2.c232f520.chunk.js"
+  },
+  {
+    "revision": "461ccc38b1135df3a5ea41a31671772d",
+    "url": "/react-website-nasa/static/js/2.c232f520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a2fcf1145974d29292",
+    "url": "/react-website-nasa/static/js/main.3a0e4147.chunk.js"
+  },
+  {
+    "revision": "a5c75714b05e19315b03",
+    "url": "/react-website-nasa/static/js/runtime-main.2bbf1460.js"
+  },
+  {
+    "revision": "c658a76944745b7cb79b2ec51a2e3c76",
+    "url": "/react-website-nasa/static/media/space.c658a769.jpg"
+  }
+]);
